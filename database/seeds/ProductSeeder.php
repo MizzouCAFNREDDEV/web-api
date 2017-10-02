befb0b1f92ce045906d069d1634831a81bbb4de9
Michael Smith <mdsf3f@mail.missouri.edu>
2017-10-01 23:12:26 -0500

    Initial project setup. Product system created

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $filename = storage_path("/seed-files/products.csv");
+        $data = $this->csvToArray($filename);
+        foreach($data as $product){
+            Product::create($product);
+        }
+    }
+
+    function csvToArray($filename = '', $delimiter = ',')
+    {
+        if (!file_exists($filename) || !is_readable($filename))
+            return false;
+
+        $header = null;
+        $data = array();
+        if (($handle = fopen($filename, 'r')) !== false)
+        {
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false)
+            {
+                if (!$header)
+                    $header = $row;
+                else
+                    $data[] = array_combine($header, $row);
+            }
+            fclose($handle);
+        }
+
+        return $data;
+    }
+}
